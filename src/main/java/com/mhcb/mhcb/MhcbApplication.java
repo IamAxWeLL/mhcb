1a30b66df4b69ea511cca3bf5226ad357af0dee7
YaroslavKlochko <yaroklochko@gmail.com>
2021-03-24 19:10:06 -0300

    gitignore added to the project

---
+++ src/main/java/com/mhcb/mhcb/MhcbApplication.java
@@ -0,0 +1,13 @@
+package com.mhcb.mhcb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhcbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MhcbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mhcb/mhcb/MhcbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhcb.mhcb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhcbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
